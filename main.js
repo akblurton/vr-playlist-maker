263df360d0a9cc147a70d4dcd0de493bc28b50d1
Ashley Blurton <a.k@blurton.me>
2017-07-02 09:58:59 +0100

    Additional dev tools

--- main.js
+++ main.js
@@ -13,9 +13,29 @@ const WEB_ASSETS_DIR = path.join(__dirname, "web");
 
 const asset = file => path.join(WEB_ASSETS_DIR, file);
 
-function createWindow() {
+function installExtensions() {
+  const installer = require("electron-devtools-installer");
+  const force = !!process.env.UPGRADE_EXTENSIONS;
+  const extensions = [
+    "REACT_DEVELOPER_TOOLS",
+    "REDUX_DEVTOOLS",
+  ];
+
+
+  return Promise.all(
+    extensions.map(name => installer.default(installer[name], force))
+  ).catch(console.log);
+}
+
+
+async function createWindow() {
+  if (process.env.NODE_ENV === "development") {
+    await installExtensions();
+  }
+
   // Create the browser window.
   win = new BrowserWindow({
+    show: false,
     width: WINDOW_FRAME_WIDTH,
     height: WINDOW_FRAME_HEIGHT,
     resizable: false,
@@ -24,6 +44,8 @@ function createWindow() {
     fullscreenable: false,
     title: "VR Playlist Maker",
   });
+
+  win.once("ready-to-show", () => win.show());
 
   // and load the index.html of the app.
   win.loadURL(url.format({
