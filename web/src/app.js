7cd355b27cfe0491158be9fe91f39776d8894fd5
Ashley Blurton <ashley@blend.media>
2017-07-20 09:17:02 +0100

    Potentially fix save state of reducer

--- web/src/app.js
+++ web/src/app.js
@@ -3,27 +3,33 @@ import "./styles/global.styl";
 import ReactDOM from "react-dom";
 import React from "react";
 
-import store, { loading } from "store";
+import createStore from "store";
 import { setMaster } from "helpers/redux";
 
 import { AppContainer } from "react-hot-loader";
 import App from "./components/App";
 import { Provider } from "react-redux";
 
-async function render() {
-  await loading;
+async function start() {
+  const store = await createStore();
   setMaster(store);
-  ReactDOM.render(
-    <AppContainer>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </AppContainer>,
-    document.getElementById("app")
-  );
+
+  function render() {
+    ReactDOM.render(
+      <AppContainer>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </AppContainer>,
+      document.getElementById("app")
+    );
+  }
+
+  render();
+  if (module.hot) {
+    module.hot.accept("./components/App", render);
+  }
 }
 
-render();
-if (module.hot) {
-  module.hot.accept("./components/App", render);
-}
+start();
+
--- web/src/store.js
+++ web/src/store.js
@@ -4,47 +4,72 @@ import * as storage from "redux-storage";
 import createEngine from "redux-storage-engine-electronjsonstorage";
 import filter from "redux-storage-decorator-filter";
 
+import electronStore from "electron-json-storage";
 
-import reducer from "./reducer";
+import reducer, { INITIAL_STATE } from "./reducer";
 import saga from "./saga";
 
-let engine;
-// Right now, don't save the config in dev mode
-if (process.env.NODE_ENV === "development") {
-  engine = filter(createEngine("vr-playlist-maker"), null, [
-    "playback",
-    "config",
-  ]);
-} else {
-  engine = filter(createEngine("vr-playlist-maker"), null, [
-    "playback",
-  ]);
+
+export default async function() {
+  const data = {
+    ...INITIAL_STATE,
+    ...(await new Promise(resolve => {
+      electronStore.getAll(function(err, response) {
+        if (err) {
+          resolve({});
+        } else {
+          try {
+            const parsed = JSON.parse(response["vr-playlist-maker"]);
+            resolve(parsed || {});
+          } catch (e) {
+            resolve({});
+          }
+        }
+      });
+    })),
+  };
+  let engine;
+  // Right now, don't save the config in dev mode
+  if (process.env.NODE_ENV === "development") {
+    engine = filter(createEngine("vr-playlist-maker"), null, [
+      "playback",
+      "config",
+    ]);
+  } else {
+    engine = filter(createEngine("vr-playlist-maker"), null, [
+      "playback",
+    ]);
+  }
+
+  const composeEnhancers = (
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+  );
+  const sagaMiddleware = createSagaMiddleware();
+  const storageMiddleware = storage.createMiddleware(engine);
+
+  storage.createLoader(engine);
+  console.log(data);
+  const store = createStore(
+    storage.reducer(reducer),
+    data,
+    composeEnhancers(
+      applyMiddleware(sagaMiddleware, storageMiddleware),
+    )
+  );
+
+  let task = sagaMiddleware.run(saga);
+
+
+  if (module.hot) {
+    module.hot.accept("./reducer", function() {
+      store.replaceReducer(reducer);
+    });
+    module.hot.accept("./saga", function() {
+      task.cancel();
+      task = sagaMiddleware.run(saga);
+    });
+  }
+
+  return store;
 }
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const sagaMiddleware = createSagaMiddleware();
-const storageMiddleware = storage.createMiddleware(engine);
 
-const load = storage.createLoader(engine);
-
-const store = createStore(
-  storage.reducer(reducer),
-  composeEnhancers(
-    applyMiddleware(sagaMiddleware, storageMiddleware),
-  )
-);
-
-let task = sagaMiddleware.run(saga);
-
-
-if (module.hot) {
-  module.hot.accept("./reducer", function() {
-    store.replaceReducer(reducer);
-  });
-  module.hot.accept("./saga", function() {
-    task.cancel();
-    task = sagaMiddleware.run(saga);
-  });
-}
-
-export const loading = load(store);
-export default store;
